1ecdba19398013ce2abb616ae04934b3a69d6047
ryuukinken <a319967646@outlook.com>
2023-12-30 21:09:20 +0900

    1.数字を当たってゲーム_完了

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("hello world!!!");
-    println!("数字を入力してください。");
+    println!("数字を当たってみてください。");
 
-    let random_number:i32 = rand::thread_rng().gen_range(1..=100);
+    let random_number: i32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("このランダム数字は:{}です。",random_number);
+    // println!("このランダム数字は:{}です。",random_number);
+    loop {
+        let mut guess: String = String::new();
 
-    let mut guess:String = String::new();
+        io::stdin().read_line(&mut guess).expect("読み取りできません！");
 
-    io::stdin().read_line(&mut guess).expect("読み取りできません！");
+        let guess: i32 = guess.trim().parse().expect("数字を入力してください！");
 
-    print!("入力された数字は:{}です。",guess);
+        // print!("入力された数字は:{}です。",guess);
 
+        // 乱数と入力された数字を比較する
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("TOO SMALL!"),
+            Ordering::Greater => println!("TOO BIG!"),
+            Ordering::Equal => {
+                println!("BINGO!");
+                break;
+            }
+        }
+    }
 }
